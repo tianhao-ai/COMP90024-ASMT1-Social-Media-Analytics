4c3e07f4317be12d28510489e0f5530e31388a2b
Tianhao-web <82075913+Tianhao-web@users.noreply.github.com>
2023-03-11 23:28:16 +1100

    update the file count_tweet_num so that it could produce output in correct format

--- .ipynb_checkpoints/count_tweet_num-checkpoint.py
+++ .ipynb_checkpoints/count_tweet_num-checkpoint.py
@@ -1,14 +1,10 @@
 import json
 from mpi4py import MPI
-import time
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-if rank == 0:
-    start_time = time.time()
-    
 # Load tweet data from JSON file
 with open('twitter-data-small.json') as f:
     tweets = json.load(f)
@@ -16,13 +12,13 @@ with open('twitter-data-small.json') as f:
 # Split the tweet data across processes
 chunk_size = len(tweets) // size
 start = rank * chunk_size
-end = start + chunk_size 
+end = start + chunk_size if rank < size - 1 else len(tweets)
 my_tweets = tweets[start:end]
 
 # Count the number of tweets made by each author
 counts = {}
 for tweet in my_tweets:
-    author_id = tweet['data']['author_id']
+    author_id = tweet['_id']
     if author_id in counts:
         counts[author_id] += 1
     else:
@@ -44,11 +40,6 @@ if rank == 0:
     # Sort the authors by the number of tweets and present the top 10
     sorted_authors = sorted(merged_counts.items(), key=lambda x: x[1], reverse=True)
     top_authors = sorted_authors[:10]
-    max_author_id_len = max(len(str(author_id)) for author_id, _ in top_authors)
-
-    # Print the top 10 authors
-    print(f"Rank\t{'Author Id':<{max_author_id_len}}\tNumber of Tweets Made")
-    for i in range(len(top_authors)):
-        author_id, num_tweets = top_authors[i]
-        print(f"#{i+1}\t{author_id:<{max_author_id_len}}\t\t{num_tweets}")
-    print(f"Elapsed time: {time.time() - start_time:.2f} seconds\n")
+    print('Top 10 authors with the most tweets:')
+    for author_id, count in top_authors:
+        print(f'Author {author_id}: {count} tweets')
